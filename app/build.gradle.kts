62ca8978b4eea98f90f4902fd33b4a38bffcf0b3
matheusfinamor1 <matheusfinamor1@gmail.com>
2023-10-11 17:04:27 -0300

    init unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     testImplementation("junit:junit:4.13.2")
+    testImplementation ("io.mockk:mockk:1.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
